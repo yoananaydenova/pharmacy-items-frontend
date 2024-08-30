b9e249d502951b0a1ef92278741e488be0c49b2f
Yoana Naydenova <y.naydenova@gmail.com>
2024-08-30 16:29:04 +0300

    create project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import './App.css'
+import Items from './pages/Items'
+
+function App() {
+
+
+  return (
+    <>
+      <Items />
+    </>
+  )
+}
+
+export default App
