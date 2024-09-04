ba1b1a28270ac5d4db6b173cd578c4e37af38efc
Yoana Naydenova <y.naydenova@gmail.com>
2024-09-04 20:51:47 +0300

    fix Navbar paths

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,10 +3,10 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
     return (
         <nav className="navbar bg-light rounded px-5">
-            <NavLink to="/search" className="navbar-brand">Pharmacy Item App</NavLink>
+            <NavLink to="/" className="navbar-brand">Pharmacy Item App</NavLink>
             <div className="d-flex flex-row justify-content-between w-25">
 
-                <NavLink to="/search" className="nav-link" >Search</NavLink>
+                <NavLink to="/" className="nav-link" >Search</NavLink>
                 <NavLink to="/login" className="nav-link" >Login</NavLink>
             </div>
         </nav>
