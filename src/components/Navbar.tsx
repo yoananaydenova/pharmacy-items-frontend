a6238c24b7bc3d5a364dc5b4f18ef2d87a29cda9
Yoana Naydenova <y.naydenova@gmail.com>
2024-09-05 19:57:01 +0300

    fix navbar buttons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,21 @@
 import { NavLink } from "react-router-dom";
 
-const Navbar = () => {
+type NavbarProps = {isLoggedIn:boolean}
+
+
+const Navbar = ({ isLoggedIn}: NavbarProps ) => {
+
     return (
         <nav className="navbar bg-light rounded px-5">
             <NavLink to="/" className="navbar-brand">Pharmacy Item App</NavLink>
-            <div className="d-flex flex-row justify-content-between w-25">
+            <div className="d-flex flex-row justify-content-between w-25 align-items-center">
 
                 <NavLink to="/" className="nav-link" >Search</NavLink>
-                <NavLink to="/login" className="nav-link" >Login</NavLink>
+                {isLoggedIn ?
+                    <NavLink to="/" className="btn btn-outline-secondary px-2 py-1">Logout</NavLink>
+                    :
+                    <NavLink to="/login" className="btn btn-secondary px-2 py-1" >Login</NavLink>
+                }
             </div>
         </nav>
     )
