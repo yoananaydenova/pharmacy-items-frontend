927a4d9727aec265d7e33d82cd9d06a42d756dfa
Yoana Naydenova <y.naydenova@gmail.com>
2024-09-11 21:11:15 +0300

    add routes, login and register functions in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,72 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { request, setAuthHeader } from './helpers/axios_helper.ts';
+import Login from "./components/Login";
+import Navbar from "./components/Navbar";
 import Search from './components/Search'
+import { useState } from "react";
+import Dashboard from "./components/Dashboard.tsx";
 
 function App() {
 
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  const onLogin = (username: string, password: string) => {
+
+    request(
+      "POST",
+      "/login",
+      {},
+      {
+        login: username,
+        password: password
+      }).then(
+        (response) => {
+          setAuthHeader(response.data.token);
+          setIsLoggedIn(true);
+        }).catch(
+          (error) => {
+            setAuthHeader(null);
+            setIsLoggedIn(false);
+          }
+        );
+  };
+
+
+
+  const onRegister = (firstName: string, lastName: string, username: string, password: string) => {
+
+    request(
+      "POST",
+      "/register",
+      {},
+      {
+        firstName: firstName,
+        lastName: lastName,
+        login: username,
+        password: password
+      }).then(
+        (response) => {
+          setAuthHeader(response.data.token);
+          setIsLoggedIn(true);
+        }).catch(
+          (error) => {
+            setAuthHeader(null);
+            setIsLoggedIn(false);
+          }
+        );
+  };
   return (
-    <>
-      <Search />
-    </>
+    <BrowserRouter>
+      <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+      <Routes>
+        <Route element={<Search />} path="/" />
+        <Route element={<Dashboard />} path="/dashboard" />
+        <Route element={<Login onLogin={onLogin} onRegister={onRegister} />} path="/login" />
+      </Routes>
+
+    </BrowserRouter>
   )
 }
 
