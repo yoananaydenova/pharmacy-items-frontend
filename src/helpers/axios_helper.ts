0219c53e995b31144d0774056039806352fc3551
Yoana Naydenova <y.naydenova@gmail.com>
2024-09-11 21:09:29 +0300

    add parameter types in setAuthHeader function

--- src/helpers/axios_helper.ts
+++ src/helpers/axios_helper.ts
@@ -4,7 +4,7 @@ export const getAuthToken = () => {
   return window.localStorage.getItem("auth_token");
 };
 
-export const setAuthHeader = (token:string) => {
+export const setAuthHeader = (token: string | null) => {
   if (token !== null) {
     window.localStorage.setItem("auth_token", token);
   } else {
@@ -15,7 +15,12 @@ export const setAuthHeader = (token:string) => {
 axios.defaults.baseURL = "http://localhost:8080";
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
-export const request = (method:string, url:string, params:Object, data:Object) => {
+export const request = (
+  method: string,
+  url: string,
+  params: Object,
+  data: Object
+) => {
   let headers = {};
   if (getAuthToken() !== null && getAuthToken() !== "null") {
     headers = { Authorization: `Bearer ${getAuthToken()}` };
