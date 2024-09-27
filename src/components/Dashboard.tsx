f83176b6614a73fc76d5d60e7338daae99eceee1
Yoana Naydenova <y.naydenova@gmail.com>
2024-09-27 19:40:22 +0300

    Add tab menu in Dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -45,10 +45,33 @@ const Dashboard = ({ login, logout }: DashboardProps) => {
         <div className="mt-5">
 
             <div className="card" >
-                <h3 className="card-header bg-light">Favorite items:</h3>
-                <div className="card-body">
-                    <Table items={favorites} login={login} logout={logout} removeItem={removeItem} />
+                <nav>
+                    <div className="nav nav-tabs" id="nav-tab" role="tablist">
+                        <button className="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Items</button>
+                        <button className="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Searches</button>
+                        <button className="nav-link" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">Profile</button>
+                    </div>
+                </nav>
+                <div className="tab-content" id="nav-tabContent">
+                    <div className="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+                        <h3 className="card-header bg-light">Favorite Items</h3>
+                        <div className="card-body">
+                            {favorites.length > 0 ?
+                                <Table items={favorites} login={login} logout={logout} removeItem={removeItem} />
+                                : null}
+                        </div>
+                    </div>
+                    <div className="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+                        <h3 className="card-header bg-light">Favorite Searches</h3>
+                        <div className="card-body">
+                            Searches
+                        </div>
+                    </div>
+                    <div className="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
+                        <h3 className="card-header bg-light">Profile Information</h3>
+                    </div>
                 </div>
+
             </div>
 
         </div>
